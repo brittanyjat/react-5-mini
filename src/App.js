ebc0e19a4eddef6df2903e866af5011ca9883a7f
brittanyjat <33502518+brittanyjat@users.noreply.github.com>
2018-02-07 14:17:08 -0700

    implemented Redux on calculator project. Created store and reducer

--- src/App.js
+++ src/App.js
@@ -1,49 +1,53 @@
 import React, { Component } from "react";
 import "./App.css";
+import { connect } from 'react-redux';
+import { increment, decrement, undo, redo } from './ducks/counter';
 
 class App extends Component {
   render() {
+    console.log(this.props)
+    const { currentValue, decrement, increment, undo, redo, futureValues, previousValues } = this.props;
     return (
       <div className="app">
         <section className="counter">
-          <h1 className="counter__current-value">{ 0 }</h1>
+          <h1 className="counter__current-value">{currentValue}</h1>
           <div className="counter__button-wrapper">
             <button
               className="counter__button increment-one"
-              onClick={ () => null }
+              onClick={() => increment(1)}
             >
               +1
             </button>
             <button
               className="counter__button increment-five"
-              onClick={ () => null }
+              onClick={() => increment(5)}
             >
               +5
             </button>
             <button
               className="counter__button decrement-one"
-              onClick={ () => null }
+              onClick={() => decrement(1)}
             >
               -1
             </button>
             <button
               className="counter__button decrement-five"
-              onClick={ () => null }
+              onClick={() => decrement(5)}
             >
               -5
             </button>
             <br />
             <button
               className="counter__button undo"
-              disabled={ true }
-              onClick={ () => null }
+              disabled={previousValues.length === 0}
+              onClick={() => undo()}
             >
               Undo
             </button>
             <button
               className="counter__button redo"
-              disabled={ true }
-              onClick={ () => null }
+              disabled={futureValues.length === 0}
+              onClick={() => redo()}
             >
               Redo
             </button>
@@ -51,7 +55,7 @@ class App extends Component {
         </section>
         <section className="state">
           <pre>
-            { JSON.stringify( this.props, null, 2 ) }
+            {JSON.stringify(this.props, null, 2)}
           </pre>
         </section>
       </div>
@@ -59,4 +63,8 @@ class App extends Component {
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return state;
+}
+
+export default connect(mapStateToProps, { increment, decrement, undo, redo })(App);
